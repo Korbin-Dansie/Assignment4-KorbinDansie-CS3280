6ce36a60ff691c2c247b24dcadd6bbc4f69c05ab
Korbin Dansie <dansiekm@gmail.com>
2023-02-07 14:53:35 -0700

    Started creating the model class

--- Tic-Tac-Toe/TicTacToe.cs
+++ Tic-Tac-Toe/TicTacToe.cs
@@ -6,8 +6,30 @@ using System.Threading.Tasks;
 
 namespace Tic_Tac_Toe
 {
-
     public class TicTacToe
     {
+        #region Attributes
+        #endregion Attributes
+
+        private string[,] saBoard;
+        private int iPlayer1Wins;
+        private int iPlayer2Wins;
+        private int iTies;
+
+        #region Constructor
+        #endregion Constructor
+
+        #region Methods
+        public bool IsWinningMove()
+        {
+            return false;
+        }
+
+        public bool IsTie()
+        {
+            return false;
+        }
+        #endregion Methods
+
     }
 }
