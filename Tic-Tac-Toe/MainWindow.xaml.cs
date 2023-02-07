7d29e2cada715a717fa4506593ffe805b1d0bc51
Korbin Dansie <dansiekm@gmail.com>
2023-02-07 14:53:23 -0700

    Scoreboard button are now working

--- Tic-Tac-Toe/MainWindow.xaml.cs
+++ Tic-Tac-Toe/MainWindow.xaml.cs
@@ -15,16 +15,52 @@ using System.Windows.Shapes;
 
 namespace Tic_Tac_Toe
 {
-
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        TicTacToe ticTacToe;
+
         public MainWindow()
         {
             InitializeComponent();
+            ticTacToe = new TicTacToe();
         }
+
+        // CLick stat game reset game, set game state to start
+
+        // Click scoareboard 
+        private void PlayerMoveClick(object sender, RoutedEventArgs e)
+        {
+           string messageBoxText = "Do you want to save changes?";
+            string caption = "Word Processor";
+            MessageBoxButton button = MessageBoxButton.YesNoCancel;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            MessageBoxResult result;
+
+            result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+        }
+
+        private void Button_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void btnTopLeft_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            btnTopLeft.Background = Brushes.Blue;
+        }
+
+        // LoadBoard - Set the content of the scoareboard based off of TicTacToeClass
+
+        // Reset Colors
+
+
+
     }
 }
